bffa9a4ec296016d48164eb52e039c43171eb012
DreamTexX <kevin.cebulla@dreamtexx.email>
2024-04-19 17:38:27 +0000

    chore: setup

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,54 @@
+const puns = [
+    "Arroganz ist so groß, dass selbst ihre Schatten einen Rückzieher machen.",
+    "Arroganz ist wie ein boomerang - egal wie weit sie ihn wirft, er kommt immer zurück.",
+    "Arroganz ist so hoch, dass selbst ihre Selbstzweifel eitel sind.",
+    "Arroganz ist wie ein Luftballon - aufgeblasen und bereit, jeden Moment zu platzen.",
+    "Arroganz ist so offensichtlich, dass sie eine eigene Eintrittskarte zur Überheblichkeit verdient hat.",
+    "Arroganz ist wie ein peinlicher Tanz - alle schauen zu, aber keiner will mitmachen.",
+    "Arroganz ist so dick, dass selbst ihr Ego gelegentlich darum bettelt, Platz zu machen.",
+    "Arroganz ist wie ein schlechtes Parfüm - überall, wo sie auftaucht, hinterlässt sie einen unangenehmen Nachgeschmack.",
+    "Arroganz ist so offensichtlich, dass sogar Spiegel sich weigern, ihr Spiegelbild zurückzugeben.",
+    "Arroganz ist wie eine unaufhaltsame Lawine - selbst wenn sie versucht, sie zu verbergen, rollt sie unaufhaltsam voran.",
+    "Arroganz ist so hartnäckig, dass selbst ein Schlag ins Gesicht sie nicht erschüttern würde.",
+    "Arroganz ist wie ein schwarzes Loch - es verschlingt jedes Kompliment und spuckt nur noch mehr Überheblichkeit aus.",
+    "Arroganz ist so groß, dass sie die Gravitation um sie herum zu beeinflussen scheint.",
+    "Arroganz ist wie ein schlecht abgestimmtes Klavier - selbst wenn sie versucht, leise zu sein, hört man sie noch in der ganzen Stadt.",
+    "Arroganz ist so strahlend, dass selbst die Sonne manchmal neidisch wird.",
+    "Arroganz ist wie ein gestrandeter Wal - riesig, unbequem und niemand weiß wirklich, wie er dorthin gekommen ist.",
+    "Arroganz ist so groß, dass selbst der Himmel manchmal nach unten schaut und seufzt.",
+    "Arroganz ist wie ein schlecht gemachtes Tattoo - einmal da, immer peinlich.",
+    "Arroganz ist so offensichtlich, dass selbst Hunde ihren Geruchssinn beleidigt finden.",
+    "Arroganz ist wie ein verschlossenes Buch - man muss es nicht öffnen, um zu wissen, dass es leer ist.",
+    "Arroganz ist so hartnäckig, dass selbst Unkrautvernichter sie nicht besiegen können.",
+    "Arroganz ist wie ein Sturm im Wasserglas - viel Lärm, aber letztendlich bedeutungslos.",
+    "Arroganz ist so groß, dass selbst ihr Schatten manchmal denkt, er sei zu viel.",
+    "Arroganz ist wie ein unsichtbarer Rucksack - sie trägt ihn überallhin, ohne es zu merken, aber alle anderen spüren das Gewicht.",
+    "Arroganz ist so groß, dass selbst ihre Träume manchmal denken, sie wären zu hochfliegend.",
+    "Arroganz ist wie eine verlorene Fernbedienung - selbst wenn sie versucht, sie zu finden, ändert sich nichts an ihrem Programm.",
+    "Arroganz ist so hartnäckig, dass selbst ein Uhrmacher sie nicht justieren könnte.",
+    "Arroganz ist wie ein schlechtes Haustier - sie liebt es, sie zu zeigen, aber niemand sonst mag sie wirklich.",
+    "Arroganz ist so groß, dass selbst ihr Schicksal manchmal den Kopf schüttelt.",
+    "Arroganz ist wie ein schlechter Witz - nur sie lacht darüber, während alle anderen die Augen verdrehen.",
+    "Arroganz ist so hartnäckig, dass selbst ein Elefant sie nicht vergessen könnte.",
+    "Arroganz ist wie eine unsichtbare Krone - sie trägt sie, aber keiner sieht den Glanz."
+];
+
+export function load({ request }) {
+    const host =
+        request.headers.get("X-Forwarded-Host") ?? request.headers.get("Host") ?? "arroganz.net";
+    let name = host.split(".").reverse().at(2);
+    if (!name) {
+        name = "Deine";
+    } else if (name.endsWith("s")) {
+        name = `${name.substring(0, name.length - 1)}'s`;
+    } else {
+        name = `${name}'s`;
+    }
+    name = name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
+
+    return {
+        name,
+        pun: `${name} ${puns[Math.floor(Math.random() * puns.length)]}`,
+        host
+    };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+    export let data;
+
+    const url = `https://${data.host}/`;
+    const title = `${data.name} Arroganz...`;
+    const description = data.pun;
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+
+    <meta name="title" content={title} />
+    <meta name="description" content={description} />
+    <meta name="keywords" content="arroganz" />
+    <meta name="robots" content="noindex, nofollow" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="language" content="German" />
+    <meta name="author" content="DreamTexX" />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content={url} />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content={description} />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content={url} />
+    <meta property="twitter:title" content={title} />
+    <meta property="twitter:description" content={description} />
+</svelte:head>
+
+<main>
+    <h1>{description}</h1>
+</main>
+
+<style>
+    :global(body) {
+        margin: 0;
+        overflow: hidden;
+    }
+
+    :global(*) {
+        box-sizing: border-box;
+    }
+
+    main {
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 20px;
+    }
+
+    h1 {
+        font-size: 1.8em;
+        font-weight: 900;
+        font-family: sans-serif;
+        text-align: center;
+        max-width: 1000px;
+    }
+</style>
